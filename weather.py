87c7c7befe73f70bae29bbdd7345830c0f42fbc6
WWW <119711067+WWW-boop@users.noreply.github.com>
2024-01-27 17:45:35 +0700

    Add Convert temperature

--- weather.py
+++ weather.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template,request,abort
+import json
+import urllib.request
+
 
 app = Flask(__name__)
-
+def tocelcius(temp):
+    return str(round(float(temp) - 273.16,2))
 
 @app.route('/')
 def hello():
