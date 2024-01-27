ea355bf83996ff3830ddc57b05c679eabe186a43
WWW <119711067+WWW-boop@users.noreply.github.com>
2024-01-27 17:58:22 +0700

    Add Api-openweathermap

--- weather.py
+++ weather.py
@@ -7,6 +7,31 @@ app = Flask(__name__)
 def tocelcius(temp):
     return str(round(float(temp) - 273.16,2))
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/',methods=['POST','GET'])
+def weather():
+    api_key = 'ef864563f67b6b77a069d7d372f19693'
+    if request.method == 'POST':
+        city = request.form['city']
+    else:
+        
+        city = 'Songkhla'
+
+    try:
+        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid='+api_key).read()
+    except:
+        return abort(404)
+    
+
+    list_of_data = json.loads(source)
+
+    
+    data = {
+        "temp": str(list_of_data['main']['temp']) + 'k',
+        "temp_cel": tocelcius(list_of_data['main']['temp']) + 'C',
+    }
+    return render_template('index.html',data=data)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
